cbd08f425925c9bba5c3997f75eb33902572e8cd
Caio de Oliveira Lopes <43317517+eredangt@users.noreply.github.com>
2020-04-06 19:10:30 -0300

    Add files via upload

---
+++ ClasseA.java
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+/*
+Trabalho Prático de Gestão de Versões - Engenharia de Software - GCC188 - 2020/01
+Nome:
+    Caio de Oliveira Lopes, 10A, 201820267.
+*/
+public class ClasseA{
+    ClasseA(){
+        private int A1;
+        private float A2;
+    }
+    
+    public void setA1(int A1){
+        this.A1 = A1;
+    }
+
+    public void setA2(float A2){
+        this.A2 = A2;
+    }
+
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    public String MA1(){
+        return "MA1";
+    }
+
+    public String MA2(){
+        return "MA2";
+    }
+}
---
+++ ClasseB.java
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+/*
+Trabalho Prático de Gestão de Versões - Engenharia de Software - GCC188 - 2020/01
+Nome:
+    Caio de Oliveira Lopes, 10A, 201820267.
+*/
+public class ClasseB{
+    ClasseB(){
+        private int B1;
+        private float B2;
+    }
+
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2){
+        this.B2 = B2;
+    }
+    
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public String MB1(){
+        return "MB1";
+    }
+
+    public String MB2(){
+        return "MB2";
+    }
+}
