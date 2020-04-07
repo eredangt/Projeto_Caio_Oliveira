c2ba52a5d7db9fe2930396644e15cfa3941ed7d5
Caio de Oliveira Lopes <caio.lopes@estudante.ufla.br>
2020-04-06 21:11:43 -0300

    Criação da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+/*
+Trabalho Prático de Gestão de Versões - Engenharia de Software - GCC188 - 2020/01
+Nome:
+    Caio de Oliveira Lopes, 10A, 201820267.
+*/
+public class ClasseC{
+    ClasseC(){
+        private String C1;
+        private int C2;
+    }
+    
+    public void setC1(String C1){
+        this.C1 = C1;
+    }
+
+    public void setC2(int C2){
+        this.C2 = C2;
+    }
+
+    public String getC1(){
+        return C1;
+    }
+
+    public int getC2(){
+        return C2;
+    }
+
+    public String MC1(){
+        return "MC1";
+    }
+
+    public String MC2(){
+        return "MC2";
+    }
