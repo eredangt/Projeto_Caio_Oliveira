ffb83a22421dd537d376f2c8daee15a74422340a
Caio de Oliveira Lopes <caio.lopes@estudante.ufla.br>
2020-04-07 12:24:08 -0300

    Criação da Classe D

---
+++ ClasseD.java
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+/*
+Trabalho Prático de Gestão de Versões - Engenharia de Software - GCC188 - 2020/01
+Nome:
+    Caio de Oliveira Lopes, 10A, 201820267.
+*/
+public class ClasseC{
+    ClasseC(){
+        private int D1;
+        private float D2;
+    }
+    
+    public void setD1(int D1){
+        this.D1 = D1;
+    }
+
+    public void setD2(float D2){
+        this.D2 = D2;
+    }
+
+    public int getD1(){
+        return D1;
+    }
+
+    public float getD2(){
+        return D2;
+    }
+
+    public String MD1(){
+        return "MD1";
+    }
+
+    public String MD2(){
+        return "MD2";
+    }
+}
